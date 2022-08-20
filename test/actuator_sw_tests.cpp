493952d6a23fd9e6c8b709d9429c1217f7b23f23
Phreakester <gettyv05@gmail.com>
2022-08-19 19:33:01 -0700

    Added first dummy test for actuator class

--- test/actuator_sw_tests.cpp
+++ test/actuator_sw_tests.cpp
@@ -1,4 +1,5 @@
 #include <unity.h>
+#include <Actuator.h>
 
 void setUp(void)
 {
@@ -10,8 +11,18 @@ void tearDown(void)
 
 }
 
+void test_run_encoder_homing()
+{
+    Actuator actuator;
+    TEST_ASSERT_TRUE(actuator.encoder_homing());
+
+}
+
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
+
+    RUN_TEST(test_run_encoder_homing);
+
     UNITY_END();
 }
