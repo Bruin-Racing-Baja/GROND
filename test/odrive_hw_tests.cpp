36527bb921cd7be67fcdf2539f127effcb89b76f
Phreakester <gettyv05@gmail.com>
2022-08-21 20:29:45 -0700

    Added basic connection tests. Added native platform to platformio configuration

---
+++ test/odrive_hw_tests.cpp
@@ -0,0 +1,35 @@
+#include <unity.h>
+#include <Odrive.h>
+
+void setUp(void)
+{
+    
+}
+
+void tearDown(void)
+{
+
+}
+
+void test_connection()
+{
+    Odrive odrive(Serial1);
+    TEST_ASSERT_TRUE( 0 == odrive.init_connection() );
+}
+
+void test_vbus_value()
+{
+    Odrive odrive(Serial1);
+    float bus_voltage = odrive.get_bus_voltage();
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 24.0, bus_voltage);
+}
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_connection);
+    RUN_TEST(test_vbus_value);
+
+    UNITY_END();
+}
