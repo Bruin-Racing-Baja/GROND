057f471234646198aa4dd5cb3e09b100562577e5
Gautier404 <and.jgautier@gmail.com>
2022-08-23 22:43:05 -0700

    CAN reading test

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,32 @@
 #include <Arduino.h>
+#include <FlexCAN_T4.h>
 
-// Libraries
-#include <SPI.h>
-#include <ArduinoLog.h>
-#include <SD.h>
 
-// Classes
-#include <Constants.h>
-
-/*
-GROND GROND GROND GROND
-GROND GROND GROND GROND
-GROND GROND GROND GROND
-GROND GROND GROND GROND
-
-The main file is organized as such:
-[ Settings ]
-[ Object Declarations ]
-[ File-Scope Variable Declarations ]
-[ Control Function ]
--- Setup Function --
-  [ Serial wait (if enabled) ]
-  * Log Begins *
-  [ Object initializtions ]
-  [ Attach control function interrupt ]
-
--- Main Function --
-Nothing :)
-
-Modes:
-0 - Normal Operation
-1 - Debug Mode [Teensy Power]
-2 - Debug Mode [Main Power]
+/*Odrive configuration:
+odrv0.axis0.config.can.node_id = 0
+odrv0.axis1.config.can.node_id = 1
+odrv0.can.config.baud_rate = 250000
 */
-#define MODE 0
-
-// Startup Settings
-#define WAIT_SERIAL 1
-#define HOME_ON_STARTUP 1 // Controls index search and home
-
-// Object Declarations
-
-IntervalTimer timer;
-File log_file;
-// File-Scope Variable Declarations
-
-// Geartooth counts
-volatile unsigned long eg_count = 0;
-volatile unsigned long gb_count = 0;
-
-// Control Function ඞ
-void control_function() {
-  Serial.println(micros());
-}
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can1;
+CAN_message_t msg;
 
 void setup() {
-  if (WAIT_SERIAL) { while(!Serial) { } }
-
-  // Log file determination and initialization
-  int log_file_number = 0;
-  while (SD.exists(("log_" + String(log_file_number) + ".txt").c_str()))
-  {
-    log_file_number++;
-  }
-  String log_name = "log_" + String(log_file_number) + ".txt";
-  Serial.println("Logging at: " + log_name);
-  log_file = SD.open(log_name.c_str(), FILE_WRITE);
-  Log.begin(LOG_LEVEL, &log_file, false);
-  Log.notice("Initialization Started - Model: %d " CR, MODEL_NUMBER);
-  
-
-  // Create interrupts to count gear teeth
-  attachInterrupt(EG_PIN, [](){++eg_count;}, RISING);
-  attachInterrupt(GB_PIN, [](){++gb_count;}, RISING);
-
-  timer.begin(control_function, control_function_interval);
-  // And so it begins...
+  // put your setup code here, to run once:
+  can1.setBaudRate(250000);
 }
 
 void loop() {
-  // There should be nothing here
-  // If you put something here GROND GROND GROND GROND
+  // put your main code here, to run repeatedly:
+    if ( can1.read(msg) ) {
+    Serial.print("CAN1 "); 
+    Serial.print("MB: "); Serial.print(msg.mb);
+    Serial.print("  ID: 0x"); Serial.print(msg.id, HEX );
+    Serial.print("  EXT: "); Serial.print(msg.flags.extended );
+    Serial.print("  LEN: "); Serial.print(msg.len);
+    Serial.print(" DATA: ");
+    for ( uint8_t i = 0; i < 8; i++ ) {
+      Serial.print(msg.buf[i]); Serial.print(" ");
+    }
+    Serial.print("  TS: "); Serial.println(msg.timestamp);
+  }
 }
-
