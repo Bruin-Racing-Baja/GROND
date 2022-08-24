017b90c8359ef5031bf569ad6bf233e3762588de
Phreakester <gettyv05@gmail.com>
2022-08-24 00:19:24 -0700

    merged in odrive branch

--- lib/Constants/src/Constants.h
+++ lib/Constants/src/Constants.h
@@ -14,11 +14,21 @@ const int MODEL_NUMBER = model;
 #define dancing 13
 #endif
 
-// Car-Indepenedent Bools
-const int LOG_LEVEL = LOG_LEVEL_NOTICE;
+// Car-Indepenedent Constants
+const int LOG_LEVEL = 0;
 
 // Car-Independent Constants
-const int control_function_interval = 1e6;   // in microseconds
+const int control_function_interval = 1e6;  //microseconds
+const int ODRIVE_BAUD_RATE = 115200;        //hz
+const int ACTUATOR_AXIS = 0;
 
+// Odrive constants
+const int ODRIVE_DEFAULT_TIMEOUT = 1000; //ms
+
+// Odrive state constants
+const int ODRIVE_UNKNOWN_STATE = 0;
+const int ODRIVE_IDLE_STATE = 1;
+const int ODRIVE_ENCODER_INDEX_SEARCH_STATE = 6;
+const int ODRIVE_VELOCITY_CONTROL_STATE = 8;
 
 #endif
---
+++ lib/ODrive/src/Odrive.h
@@ -0,0 +1,36 @@
+#ifndef odrive_h
+#define odrive_h
+
+#include <HardwareSerial.h>
+
+class Odrive
+{
+    public:
+        Odrive(HardwareSerial& serial);
+        bool init_connection();
+        bool set_velocity(float velocity, int axis);
+        bool encoder_index_search(int axis);
+        float get_bus_voltage();
+        bool idle(int axis);
+
+        // Debugging / testing functions
+        int get_state(int axis);
+        int get_encoder_count(int axis);
+        ~Odrive();
+
+    private:
+        int status;
+        int axis_state[2];
+
+        bool set_state(int state, int axis);
+
+        // Helper functions to read Odrive messages
+        String read_string();
+        float read_float();
+        int32_t read_int();
+
+
+        HardwareSerial& odrive_serial;
+};
+
+#endif
