f3776a98d131e6a4ee0fdcbbc81a800291d02cf5
Phreakester <gettyv05@gmail.com>
2023-04-21 01:30:38 -0700

    Added state manager to oled class and made a method to display ity

--- lib/OLED/src/OLED.cpp
+++ lib/OLED/src/OLED.cpp
@@ -2,20 +2,23 @@
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 #include <OLED.h>
+#include <header_message.pb.h>
+#include <log_message.pb.h>
+#include <pb.h>
+#include <pb_common.h>
 
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
+// The pins for I2C are defined by the Wire-library.
 // On an arduino UNO:       A4(SDA), A5(SCL)
 // On an arduino MEGA 2560: 20(SDA), 21(SCL)
 // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS \
+  0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 int scrollNum = 0;
@@ -23,32 +26,47 @@ int ptrPos = 0;
 unsigned long ss_m = 0;
 
 bool OLED::init() {
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) return 0;
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    return 0;
   display.display();
-  delay(2000); // Pause for 2 seconds
+  delay(2000);  // Pause for 2 seconds
   display.clearDisplay();
   display.display();
   return 1;
 }
 
 bool OLED::refresh() {
-    display.clearDisplay();
-//    setFormat();
-//    display.println("Runtime: "+String((millis()-ss_m)/1000.0));
-    display.display();
-    return 1;
+  display.clearDisplay();
+  //    setFormat();
+  //    display.println("Runtime: "+String((millis()-ss_m)/1000.0));
+  display.display();
+  return 1;
 }
 
 bool OLED::printInt(int n) {
-    display.clearDisplay();
-    setFormat();
-    display.println(n);
-    display.display();
-    return 1;
+  display.clearDisplay();
+  setFormat();
+  display.println(n);
+  display.display();
+  return 1;
+}
+
+bool OLED::printDebug(LogMessage* log_message) {
+  display.clearDisplay();
+  setFormat();
+  display.println("Runtime: " +
+                  String(log_message->control_cycle_start_us / 1.0e6));
+  display.println("Cycle Count: " + String(log_message->control_cycle_count));
+  display.println("Engine RPM: " + String(log_message->engine_rpm));
+  display.println("Wheel RPM: " + String(log_message->wheel_rpm));
+  display.println("Encoder: " + String(log_message->shadow_count));
+
+  display.display();
+  return 1;
 }
 
 void OLED::setFormat() {
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
+  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setCursor(0, 0);              // Start at top-left corner
 }
--- lib/OLED/src/OLED.h
+++ lib/OLED/src/OLED.h
@@ -1,14 +1,20 @@
 #ifndef oled_h
 #define oled_h
 
-class OLED
-{
-    public:
-        bool init();
-        bool refresh();
-        bool printInt(int n);
-    private:
-        void setFormat();
+#include <header_message.pb.h>
+#include <log_message.pb.h>
+#include <pb.h>
+#include <pb_common.h>
+
+class OLED {
+ public:
+  bool init();
+  bool refresh();
+  bool printInt(int n);
+  bool printDebug(LogMessage* log_message);
+
+ private:
+  void setFormat();
 };
 
 #endif
